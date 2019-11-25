58134c24c3d6ed82147d9b468955d7070292c4d1
vadimitx <vadimitx@gmail.com>
2019-11-25 21:08:20 +0300

    chess board done )

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let chess = [
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0] // двумерный масив
+	[0,0,0,0,0,0,0,0]
 ];
 
 function draw() {
@@ -14,5 +14,57 @@ function draw() {
 	let m = 0;
 	for (let i = 0; i< chess.length; i++){
 		let arr = chess[i];
+		for (let k = 0; k < arr.length; k++){
+			if(m%2 == 0) {
+			out += `<div class="chess-block" data-x="${k}" data-y="${i}"></div>`; //createElement
+			}
+			else {
+				out += `<div class="chess-block bg-black" data-x="${k}" data-y="${i}"></div>`;
+			}
+			m++;
+		}
+		m++;
 	}
-};
+	document.querySelector('#field').innerHTML = out;
+	document.querySelectorAll('.chess-block').forEach(function(element){
+		element.onclick = horse;
+	});
+}
+draw();
+
+function horse(){
+	document.querySelectorAll('.chess-block').forEach(function(element){
+		element.classList.remove('start');
+		element.classList.remove('active')
+	})
+	let x = this.dataset.x;
+	let y = this.dataset.y;
+	console.log(x + ' ' + y);
+	//this.classList.all('start');
+	if (+x + 2 < 8 && +y + 1 < 8){
+		document.querySelector(`.chess-block[data-x="${+x + 2}"][data-y="${+y + 1}"]`).classList.add('active');
+	}
+	if (+x + 2 < 8 && +y - 1 >= 0){
+		document.querySelector(`.chess-block[data-x="${+x + 2}"][data-y="${+y - 1}"]`).classList.add('active');
+	}
+	if (+x - 2 >= 0 && +y + 1 < 8){
+		document.querySelector(`.chess-block[data-x="${+x - 2}"][data-y="${+y + 1}"]`).classList.add('active');
+	}
+	if (+x - 2 >= 0 && +y - 1 >= 0){
+		document.querySelector(`.chess-block[data-x="${+x - 2}"][data-y="${+y - 1}"]`).classList.add('active');
+	}
+	if (+x + 1 < 8 && +y - 2 >= 0){
+		document.querySelector(`.chess-block[data-x="${+x + 1}"][data-y="${+y - 2}"]`).classList.add('active');
+	}
+	if (+x - 1 >= 0 && +y - 2 >= 0){
+		document.querySelector(`.chess-block[data-x="${+x - 1}"][data-y="${+y - 2}"]`).classList.add('active');
+	}
+	if (+x + 1 < 8 && +y + 2 < 8){
+		document.querySelector(`.chess-block[data-x="${+x + 1}"][data-y="${+y + 2}"]`).classList.add('active');
+	}
+	if (+x - 1 >= 0 && +y + 2 < 8){
+		document.querySelector(`.chess-block[data-x="${+x - 1}"][data-y="${+y + 2}"]`).classList.add('active');
+	}
+}
+
+
